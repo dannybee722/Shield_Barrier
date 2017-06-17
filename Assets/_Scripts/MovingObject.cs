babcd59e9d6f8851f91295856ead350378cb1d83
Daniel Braunstein <dbraunst@umich.edu>
2017-06-17 18:04:41 -0400

    Added player animations
    Added player animations using placeholder sprites

---
+++ Assets/_Scripts/MovingObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour {
+
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+
+
+	// Use this for initialization
+	protected virtual void Start () {
+
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+
+    private void Move()
+    {
+
+    }
+}
---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MovingObject {
+
+    public Animator animator;
+
+
+    private int moveDir;
+    private float moveHoriz;
+    private float moveVert;
+
+    // Use this for initialization
+    protected override void Start () {
+        animator = GetComponent<Animator>();
+
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveHoriz = Input.GetAxisRaw("Horizontal");
+        moveVert = Input.GetAxisRaw("Vertical");
+
+        Move(moveHoriz, moveVert);
+
+        Debug.Log("Horiz " + moveHoriz);
+        Debug.Log("Vert " + moveVert);
+    }
+
+    private void Move(float horiz, float vert)
+    {
+        //if we're moving right, and up/down val is between(incl) +/- 0.5, keep animation as moving right
+        if ((horiz > 0 && (vert >= 0 && vert <= 0.5)) || (horiz > 0 && ( vert <= 0 && vert >= -0.5)))
+        {
+            animator.SetTrigger("runRight");
+        }
+
+        //if we're moving left, and up/down val is between(incl) +/- 0.5, keep animation as moving left
+        else if ((horiz < 0 && (vert >= 0 && vert <= 0.5)) || (horiz < 0 && (vert <= 0 && vert >= -0.5)))
+        {
+            animator.SetTrigger("runLeft");
+        }
+        
+        //if we're moving up, and left/right val is between(incl) +/- 0.5, keep animation as moving left
+        else if ((vert > 0 && (horiz >= 0 && horiz <= 0.5)) || (vert > 0 && (horiz <= 0 && horiz >= -0.5)))
+        {
+            animator.SetTrigger("runUp");
+        }
+        
+        //if we're moving down, and left/right val is between(incl) +/- 0.5, keep animation as moving left
+        else if ((vert < 0 && (horiz >= 0 && horiz <= 0.5)) || (vert < 0 && (horiz <= 0 && horiz >= -0.5)))
+        {
+            animator.SetTrigger("runDown");
+        }
+    }
+}
