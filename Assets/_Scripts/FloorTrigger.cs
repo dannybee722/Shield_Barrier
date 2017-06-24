dcaf0c98ff204ad5618bbc4f46d49803370e53fa
Daniel Braunstein <dbraunst@umich.edu>
2017-06-23 20:25:45 -0400

    PlayerMovement mostly done
    Worked more on player movement/ colliders. Added much more to movement test scene to experiment with layers, Sorting Layers, and collider boxes.
    
    Added FloorTrigger.cs to set the character layer/blockinglayer when going to a new 'floor' or returning to first one.
    
    STILL TO COME: Scene management + door entry

---
+++ Assets/_Scripts/FloorTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorTrigger : MonoBehaviour {
+
+    public int floorNumber;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (floorNumber == 1)
+            {
+                //set player Layer to "BlockingLayer"
+                collision.gameObject.layer = 8;
+
+                collision.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "Units";
+            }
+
+            if (floorNumber == 2)
+            {
+                //set player Layer to "BlockingLayer2"
+                collision.gameObject.layer = 11;
+
+
+                collision.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "Units2";
+            }
+        }
+    }
+}
